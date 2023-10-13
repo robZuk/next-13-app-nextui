51da4198a5aa16743e9e2d8435d1d18336f09208
Robert Aleksander <robert.aleks.zuk@gmail.com>
2023-10-13 10:12:12 +0200

    second commit

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -1,15 +1,15 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// export default prisma;
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-export default prisma;
-// import { PrismaClient } from "@prisma/client"
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
-// declare global {
-//   var prisma: PrismaClient | undefined
-// }
-
-// const client = globalThis.prisma || new PrismaClient()
-// if (process.env.NODE_ENV !== "production") globalThis.prisma = client
-
-// export default client
+export default client;
